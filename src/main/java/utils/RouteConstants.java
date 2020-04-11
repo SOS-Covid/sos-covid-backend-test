e6ba91b93bdbf52be06580b21142b4a7daed358b
Arthur Guterres Boeck <arthur_boeck@sicredi.com.br>
2020-04-11 20:29:52 -0300

    :triangular_flag_on_post: Base URI updated

--- src/main/java/utils/RouteConstants.java
+++ src/main/java/utils/RouteConstants.java
@@ -2,7 +2,7 @@ package utils;
 
 public class RouteConstants {
 
-    public static final String URL = "http://sos-covid-backend.herokuapp.com";
+    public static final String URL = "https://coopsolidaria.herokuapp.com";
 
     public static final String REGISTER_USER = "/user/register";
     public static final String GET_CONTRIBUIDOR = "/user/find/contributor";
